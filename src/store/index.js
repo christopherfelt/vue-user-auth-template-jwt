bbbb3b7ea35488ea0c46d03080fce9b2b710f682
chris_MSI1 <chris.h.felt@gmail.com>
2020-07-02 22:45:45 -0600

    init

--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,105 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+import Vue from "vue";
+import Vuex from "vuex";
+import axios from "axios";
+import VueRouter from "vue-router";
+import router from "../router/index";
 
-Vue.use(Vuex)
+Vue.use(Vuex);
+
+axios.defaults.xsrfCookieName = "csrftoken";
+axios.defaults.xsrfHeadername = "X-CSRFToken";
+
+// let base = window.location.host.includes("localhost")
+//   ? "//localhost:3000/"
+//   : "/";
+
+// let api = axios.create({
+//   baseURL: base + "api/",
+//   timeout: 3000,
+//   withCredentials: true,
+// });
 
 export default new Vuex.Store({
   state: {
+    authUser: {},
+    isAuthenticated: false,
+    jwt: localStorage.getItem("token"),
+    endpoints: {
+      obtainJWT: "http://127.0.0.1:8000/auth/obtain_token/",
+      refrshJWT: "http://127.0.0.1:8000/auth/refresh_token/",
+      baseUrl: "http://127.0.0.1:8000/",
+    },
   },
   mutations: {
+    setAuthUser(state, { authUser, isAuthenticated }) {
+      Vue.set(state, "authUser", authUser);
+      Vue.set(state, "isAuthenticated", isAuthenticated);
+    },
+    updateToken(state, newToken) {
+      localStorage.setItem("token", newToken);
+      state.jwt = newToken;
+    },
+    removeToken(state) {
+      localStorage.removeItem("token");
+      state.jwt = null;
+    },
   },
   actions: {
+    async login({ commit, dispatch }, userData) {
+      try {
+        let res = await axios.post(this.state.endpoints.obtainJWT, userData);
+        commit("updateToken", res.data.token);
+        dispatch("getUserData");
+      } catch (error) {
+        console.error(error);
+      }
+    },
+    async getUserData({ commit, dispatch }) {
+      try {
+        let base = {
+          baseURL: this.state.endpoints.baseUrl,
+          headers: {
+            Authorization: `JWT ${this.state.jwt}`,
+            "Content-Type": "application/json",
+          },
+          xhrFields: {
+            withCredentials: true,
+          },
+        };
+        const axiosInstance = axios.create(base);
+        let res = await axiosInstance({
+          url: "/user/",
+          method: "get",
+          params: {},
+        });
+        commit("setAuthUser", {
+          authUser: res.data,
+          isAuthenticated: true,
+        });
+        router.push({ name: "Home" });
+      } catch (error) {
+        console.error(error);
+      }
+    },
+    async createNewUser({ commit, dispatch }, userData) {
+      try {
+        let res = await axios.post(
+          this.state.endpoints.baseUrl + "user/create/",
+          userData
+        );
+        dispatch("login", userData);
+      } catch (error) {
+        console.error(error);
+      }
+    },
+    async logout({ commit, dispatch }) {
+      commit("setAuthUser", {
+        authUser: {},
+        isAuthenticated: false,
+      });
+      commit("removeToken");
+      router.push({ name: "Home" });
+    },
   },
-  modules: {
-  }
-})
+  modules: {},
+});
---
+++ src/views/Login.vue
@@ -0,0 +1,121 @@
+<template lang="html">
+  <div>
+    <h1>Login</h1>
+    <form class="login form">
+      <div class="field">
+        <label for="login-username">Username</label>
+        <input
+          v-model="loginUsername"
+          type="text"
+          placeholder="Username"
+          autofocus="autofocus"
+          maxlength="150"
+          id="login-username"
+        />
+      </div>
+      <div class="field">
+        <label for="login-password">Password</label>
+        <input
+          v-model="loginPassword"
+          type="password"
+          placeholder="Password"
+          id="login-password"
+        />
+      </div>
+      <button
+        @click.prevent="authenticate"
+        class="button primary"
+        type="submit"
+      >
+        Log In
+      </button>
+    </form>
+    <br />
+    <h1>Sign Up</h1>
+    <form class="signup form">
+      <div class="field">
+        <label for="signup-username">Username</label>
+        <input
+          v-model="signupUsername"
+          type="text"
+          placeholder="Username"
+          autofocus="autofocus"
+          maxlength="150"
+          id="signup-username"
+        />
+      </div>
+      <div class="field">
+        <label for="signup-email">Email</label>
+        <input
+          v-model="signupEmail"
+          type="email"
+          placeholder="email"
+          id="signup-email"
+        />
+      </div>
+      <div class="field">
+        <label for="signup-password">Password</label>
+        <input
+          v-model="signupPassword"
+          type="password"
+          placeholder="Password"
+          id="signup-password"
+        />
+      </div>
+      <small style="color: red;" v-show="signupPassword != signupPasswordCheck"
+        >Password Don't Match</small
+      >
+      <div class="field">
+        <label for="signup-password-check">Password</label>
+        <input
+          v-model="signupPasswordCheck"
+          type="password"
+          placeholder="Password"
+          id="signup-password-check"
+        />
+      </div>
+      <button @click.prevent="signup" class="button primary" type="submit">
+        Sign Up
+      </button>
+    </form>
+    <br />
+    <button @click.prevent="logout">Logout</button>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  data() {
+    return {
+      loginUsername: "",
+      loginPassword: "",
+      signupUsername: "",
+      signupEmail: "",
+      signupPassword: "",
+      signupPasswordCheck: "",
+    };
+  },
+  methods: {
+    authenticate() {
+      this.$store.dispatch("login", {
+        username: this.loginUsername,
+        password: this.loginPassword,
+      });
+    },
+    signup() {
+      this.$store.dispatch("createNewUser", {
+        username: this.signupUsername,
+        email: this.signupEmail,
+        password: this.signupPassword,
+      });
+    },
+    logout() {
+      this.$store.dispatch("logout");
+    },
+  },
+};
+</script>
+
+<style lang="css"></style>
